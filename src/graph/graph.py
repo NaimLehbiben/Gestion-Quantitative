6f488bd593db9b9c9c50e4f33d5d59563c6ae1f5
nlehbiben <naim.lehbiben@dauphine.eu>
2024-05-05 11:49:27 +0200

    Nettoyage

--- src/graph/graph.py
+++ src/graph/graph.py
@@ -5,12 +5,9 @@ import pandas as pd
 def plot_time_series(df, column_names, title, xlabel, ylabel):
     plt.figure(figsize=(10, 6))
     for column_name in column_names:
-        plt.plot(df['index'], df[column_name], label=column_name)  
+        plt.plot(df.index, df[column_name], label=column_name)
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
-    plt.legend() 
-    plt.grid(True)
-    plt.ylim(bottom=0)
-    plt.xlim(left=df['index'].min(), right=df['index'].max())
+    plt.legend()
     plt.show()
